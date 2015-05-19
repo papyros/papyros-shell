bb3d32257c21cf6743b3647591431f38fd2aeeff
Elias Aebi <user142@hotmail.com>
2015-05-19 18:51:23 +0200

    [DesktopFile] Avoid double slashes in paths
    - Closes #94

--- backend/desktop/desktopfile.cpp
+++ backend/desktop/desktopfile.cpp
@@ -62,8 +62,8 @@ QString DesktopFile::pathFromAppId(QString appId)
 {
     QStringList paths;
     paths << "~/.local/share/applications"
-          << "/usr/local/share/applications/"
-          << "/usr/share/applications/";
+          << "/usr/local/share/applications"
+          << "/usr/share/applications";
 
     return findFileInPaths(appId + ".desktop", paths);
 }
